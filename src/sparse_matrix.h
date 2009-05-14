244f96a9823306fa3afbcdb517fddb4a605fbe30
Andreas Biegert <andreas@andreaspc.(none)>
2009-05-14 17:08:21 +0200

    Minor changes.

--- src/sparse_matrix.h
+++ src/sparse_matrix.h
@@ -14,7 +14,7 @@ using google::sparsetable;
 template<class T>
 class sparse_matrix {
  public:
-  // public typedefs
+  // Public typedefs
   typedef T value_type;
   typedef sparse_matrix self;
   typedef typename sparsetable<T>::iterator iterator;
@@ -26,13 +26,13 @@ class sparse_matrix {
   typedef typename sparsetable<T>::const_iterator const_row_type;
   typedef stride_iter< typename sparsetable<T>::const_iterator > const_col_type;
 
-  // constructors
+  // Constructors
   sparse_matrix() : num_rows_(0), num_cols_(0), m_() {}
   sparse_matrix(int r, int c) : num_rows_(r), num_cols_(c), m_(r * c) {}
   sparse_matrix(const self& x)
-  : num_rows_(x.num_rows_), num_cols_(x.num_cols_), m_(x.m_) {}
+      : num_rows_(x.num_rows_), num_cols_(x.num_cols_), m_(x.m_) {}
 
-  // allow construction from matricies of other types
+  // Allow construction from matricies of other types
   template<typename New_T>
   explicit sparse_matrix(const sparse_matrix<New_T>& x)
       : num_rows_(x.num_rows_), num_cols_(x.num_cols_), m_(x) {}
@@ -43,7 +43,7 @@ class sparse_matrix {
   int size() const { return num_rows_ * num_cols_; }
   int num_nonempty() const { return m_.num_nonempty(); }
 
-  // element access
+  // Element access
   row_type row_begin(int n) { return m_.begin() + n * num_cols(); }
   row_type row_end(int n) { return row_begin(n) + num_cols(); }
   col_type col_begin(int n) { return col_type(m_.begin() + n, num_cols()); }
@@ -73,7 +73,7 @@ class sparse_matrix {
     num_cols_ = c;
   }
 
-  // operators
+  // Operators
   self& operator=(const self& x) {
     m_.resize(x.size());
     m_ = x.m_;
